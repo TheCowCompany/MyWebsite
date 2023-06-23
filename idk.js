b55fb8400ab8c9a4df580b643b4ece95e705a699
Sukhnain Deol <dsukhnain@gmail.com>
2023-06-23 03:29:55 -0700

    started

--- idk.js
+++ idk.js
@@ -1,2 +1,12 @@
-// priint apples
-console.log('apples');
+document.addEventListener("DOMContentLoaded", function() {
+    var button = document.getElementById("playButton");
+  
+    // Add a click event listener to the button
+    button.addEventListener("click", function() {
+      // Perform the action you want when the button is clicked
+  
+      // Send a message
+      var message = "Yo";
+      alert(message);
+    });
+  });
