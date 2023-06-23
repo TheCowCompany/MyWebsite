a694676b4a9de872a39598373ebb702e2d11fb7f
Sukhnain Deol <dsukhnain@gmail.com>
2023-06-23 05:18:12 -0700

    added secondary game page

--- js/idk.js
+++ js/idk.js
@@ -7,6 +7,19 @@ document.addEventListener("DOMContentLoaded", function() {
   
       // Send a message
       var message = "Yo";
-      alert(message);
+      
     });
   });
+
+function loadHTMLFile(file) {
+  var xhr = new XMLHttpRequest();
+  xhr.open("GET", file, true);
+  xhr.onreadystatechange = function () {
+      if (xhr.readyState === 4 && xhr.status === 200) {
+          document.open();
+          document.write(xhr.responseText);
+          document.close();
+      }
+  };
+  xhr.send();
+}
