a0466e9fcb416bc3c8256e1932b13ebf702a0e36
Sukhnain Deol <dsukhnain@gmail.com>
2023-06-23 05:29:06 -0700

    remove alert function

--- js/idk.js
+++ js/idk.js
@@ -1,16 +1,3 @@
-document.addEventListener("DOMContentLoaded", function() {
-    var button = document.getElementById("playButton");
-  
-    // Add a click event listener to the button
-    button.addEventListener("click", function() {
-      // Perform the action you want when the button is clicked
-  
-      // Send a message
-      var message = "Yo";
-      
-    });
-  });
-
 function loadHTMLFile(file) {
   var xhr = new XMLHttpRequest();
   xhr.open("GET", file, true);
